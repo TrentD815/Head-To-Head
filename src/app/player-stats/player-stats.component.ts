090e6d86492be290015fae55a8dda4ba7e21a0c4
Trent Davis <trentdavis815@yahoo.com>
2021-08-19 23:02:40 -0400

    Progress on basic stats board UI

--- src/app/player-stats/player-stats.component.ts
+++ src/app/player-stats/player-stats.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { PlayerStats} from "../player-stats";
 
 @Component({
   selector: 'player-stats',
   templateUrl: './player-stats.component.html',
-  styleUrls: ['./player-stats.component.scss']
+  styleUrls: ['./player-stats.component.scss', '../app.component.scss']
 })
 export class PlayerStatsComponent implements OnInit {
-  playerIdentity ?: string;
+  @Input() playerIdentity ?: string;
   playerStats ?: PlayerStats;
+
+  // Compares players head to head stats for each stat to see whose was better
+  async determineBetterStat(player1: PlayerStats, player2: PlayerStats){
+
+  }
+  // Apply styling based on the winner of a stat to see visually who has a better stat a bit faster
+  async applyWinnerOrLoserStatColor(statBar:any) {
+
+  }
   constructor() { }
 
   ngOnInit(): void {
