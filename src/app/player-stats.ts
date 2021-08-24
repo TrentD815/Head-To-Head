3ebb40d80d6eb170b66767e75242921bec0d5d82
Trent Davis <trentdavis815@yahoo.com>
2021-08-23 21:33:38 -0400

    Add more stat fields

--- src/app/player-stats.ts
+++ src/app/player-stats.ts
@@ -1,9 +1,23 @@
 export interface PlayerStats {
+  gamesPlayed ?: number;
+  averageMinutesPlayed ?: number;
+  averageFieldGoalsMade ?: number;
+  averageFieldGoalsAttempted ?: number;
+  averageFieldGoalPercentage ?: number;
+  average3PointersMade ?: number;
+  average3PointersAttempted ?: number;
+  average3PointPercentage ?: number;
+  averageFreeThrowsMade ?: number;
+  averageFreeThrowPercentage ?: number;
+  averageFreeThrowsAttempted ?: number;
+  averageOffensiveRebounds ?: number;
+  averageDefensiveRebounds ?: number;
+  averageTurnovers ?: number;
   averagePoints ?: number;
   averageRebounds ?: number;
   averageAssists ?: number;
   averageSteals ?: number;
   averageBlocks ?: number;
   plusMinus ?: number;
-
+  averagePersonalFouls ?: number;
 }
--- src/app/player-stats/player-stats.component.ts
+++ src/app/player-stats/player-stats.component.ts
@@ -23,19 +23,49 @@ export class PlayerStatsComponent implements OnInit {
 
   ngOnInit(): void {
       this.player1Stats = {
-        averagePoints: 25.1,
+        gamesPlayed: 45,
+        averageMinutesPlayed: 33.4,
+        averagePoints: 25.0,
         averageAssists: 7.8,
         averageRebounds: 7.7,
         averageBlocks: .6,
         averageSteals: 1.1,
+        averageFieldGoalsMade: 9.4,
+        averageFieldGoalsAttempted: 18.3,
+        averageFieldGoalPercentage: 51.3,
+        average3PointersMade: 2.3,
+        average3PointersAttempted: 6.3,
+        average3PointPercentage: 36.5,
+        averageFreeThrowsMade: 4.0,
+        averageFreeThrowsAttempted: 5.7,
+        averageFreeThrowPercentage: 69.8,
+        averageOffensiveRebounds: .6,
+        averageDefensiveRebounds: 7.0,
+        averageTurnovers: 3.7,
+        averagePersonalFouls: 1.6,
         plusMinus: 6.4
       }
       this.player2Stats = {
+        gamesPlayed: 61,
+        averageMinutesPlayed: 33.0,
         averagePoints: 28.1,
         averageAssists: 5.9,
-        averageRebounds: 11,
+        averageRebounds: 11.0,
         averageBlocks: 1.2,
         averageSteals: 1.2,
+        averageFieldGoalsMade: 10.3,
+        averageFieldGoalsAttempted: 18.0,
+        averageFieldGoalPercentage: 56.9,
+        average3PointersMade: 1.1,
+        average3PointersAttempted: 3.6,
+        average3PointPercentage: 30.3,
+        averageFreeThrowsMade: 6.5,
+        averageFreeThrowsAttempted: 9.5,
+        averageFreeThrowPercentage: 68.5,
+        averageOffensiveRebounds: 1.6,
+        averageDefensiveRebounds: 9.4,
+        averageTurnovers: 3.4,
+        averagePersonalFouls: 2.8,
         plusMinus: 6.7
       }
   }
