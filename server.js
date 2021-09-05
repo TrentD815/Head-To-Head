70344330a18192d2c2e6b25f6482eb84f9c51ba2
Trent Davis <trentdavis815@yahoo.com>
2021-09-05 11:08:00 -0400

    Fix heroku deployment

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT ? process.env.PORT : 8080;
 
-app.use(express.static(__dirname + '/dist/head-to-head'));
+app.use(express.static(__dirname + '/dist/Head-To-Head'));
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/head-to-head/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/Head-To-Head/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(port || 8080);
+app.listen(port);
 console.log(`Server listening on port ${port}`)
